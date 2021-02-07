749b94b8882ac54327b63ec0007d81dd61f70ab8
Mike Haboustak <haboustak@gmail.com>
2021-02-07 13:50:35 -0500

    Add list_del impl

--- include/list.h
+++ include/list.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <stdint.h>
+#include <stdbool.h>
 
 struct list_item {
     uint32_t    id;
+    bool        present;
 }
 
 void list_add(struct list_item *item);
+void list_del(struct list_item *item);
--- list/list.c
+++ list/list.c
@@ -5,4 +5,11 @@ static struct list_item m_list = {};
 void list_add(struct list_item *item)
 { 
     m_list.id = item->id;
+    m_list.present = true;
+}
+
+void list_del(struct list_item *item)
+{
+    m_list.id = 0;
+    m_list.present = false;
 }
